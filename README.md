2ae2f5e48d9d5aca388fa4eddd7b138e4cec6948
Matt Dawson <mdawsondev@gmail.com>
2018-03-12 16:56:47 -0400

    Add first version

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# scribd-unlimited
-Removes advertisements and unblurs content.
+# Scribd Unlimited
+
+Removes Scribd advertisements and unblurs content via TamperMonkey by injecting code to remove two DOM classes. You need to install [TamperMonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) for Chrome, or [GreaseMonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) to inject this script to the webpage.
---
+++ script.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         Scribd Unlimited
+// @namespace    https://tampermonkey.net/
+// @version      1.0.0
+// @description  Removes advertisements and unblurs pages.
+// @author       Matt Dawson | https://mdawsondev.com
+// @icon         https://pbs.twimg.com/profile_images/864156483881123840/wFgIx3RN_400x400.jpg
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.16.0/polyfill.js
+// @include      http://*.scribd.com/document/*
+// @include      https://*.scribd.com/document/*
+// ==/UserScript==
+
+/* jshint ignore:start */
+var inline_src = (<><![CDATA[
+  /* jshint ignore:end */
+  /* jshint esnext: false */
+  /* jshint esversion: 6 */
+
+  (function unblurDoc() {
+      const adEles   = document.querySelectorAll('.page_missing_explanation'),
+            blurEles = document.querySelectorAll('.blurred_page');
+
+      //blurEles.forEach(node => node.classList.remove('blurred_page'));
+      adEles.forEach(e => e.parentNode.removeChild(e));
+
+      console.log("Scribd Full Access: Scribed has been cleaned.");
+
+  })();
+
+  /* jshint ignore:start */
+]]></>).toString();
+                var c = Babel.transform(inline_src, { presets: [ "es2015", "es2016" ] });
+eval(c.code);
+/* jshint ignore:end */
